b6112d1eb283163f67ed548f7877eee60a81bbb6
Alpacorp <alejandro.palacios88@gmail.com>
2022-02-25 13:52:08 -0500

    fix width body page

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -10,8 +10,6 @@ const Video = () => {
         </figure> */}
         <iframe
           src="https://www.facebook.com/plugins/video.php?height=316&href=https%3A%2F%2Fwww.facebook.com%2FSoyMayorguista%2Fvideos%2F2781252622178229%2F&show_text=false&width=560&t=0"
-          width="560"
-          height="316"
           style={{ border: "none", overflow: "hidden" }}
           scrolling="no"
           frameborder="0"
